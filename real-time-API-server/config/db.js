f42d5be919670aeed632d1e7f369557a59833e59
Son Pham <sonpxvn@gmail.com>
2024-03-13 21:11:54 +0700

    update dockerfile

--- real-time-API-server/config/db.js
+++ real-time-API-server/config/db.js
@@ -1,26 +1,29 @@
 import mongoose from 'mongoose'
-import {DB_URL} from '../config/variables'
+import {DB_HOST} from '../config/variables'
+import {DB_USER} from '../config/variables'
+import {DB_PASSWORD} from '../config/variables'
+import {DB_NAME} from '../config/variables'
 
 // Connect & config Mysql
 var mysql = require('mysql');
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "your_user",
-  password: "your_password",
-  database: "your_database"
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!!!")
+  console.log("DB Connected!!!")
 });
 
 // end config Mysql 
 
-mongoose.connect(DB_URL, {
-  useNewUrlParser:true,
-  useUnifiedTopology:true
-}, () => {
-  console.log(DB_URL);
-  console.log(`DB up and running`);
-})
+// mongoose.connect(DB_URL, {
+//   useNewUrlParser:true,
+//   useUnifiedTopology:true
+// }, () => {
+//   console.log(DB_URL);
+//   console.log(`DB up and running`);
+// })
--- real-time-API-server/config/variables.js
+++ real-time-API-server/config/variables.js
@@ -4,10 +4,18 @@ require('dotenv').config();
 
 dotenv.config()
 
-const DB_URL = `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_DBNAME}`;
+// const DB_URL = `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_DBNAME}`;
 const PORT = process.env.PORT;
+const DB_HOST = `${process.env.MYSQL_DB_URL}`;
+const DB_USER = `${process.env.MYSQLDB_USER}`;
+const DB_NAME = `${process.env.MYSQLDB_DATABASE}`;
+const DB_PASSWORD = `${process.env.MYSQLDB_ROOT_PASSWORD}`;
+
 
 export {
-  DB_URL,
-  PORT
+  DB_HOST,
+  PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
 }
