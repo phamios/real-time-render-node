1364d09897f0c76dc3acf695d8b1450923ea5356
Miracle <Miracleiztb@gmail.com>
2024-03-14 16:21:52 +0700

    refactor

--- real-time-API-client/src/components/App.js
+++ real-time-API-client/src/components/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 // import Orders from './Orders'
-import '../index.css'
-import Leaflet from "../Leaflet"
+import "../index.css";
+// import "./App.css";
+import Leaflet from "./Leaflet";
+import SideBar from "./SideBar";
 
 export default function App() {
   return (
-    <div className='app'>
-      <h1 className='title'>Store</h1>
-        {/* <Orders /> */}
-        <Leaflet />
+    <div className="app">
+      {/* <h1 className="title">Store</h1> */}
+      {/* <Orders /> */}
+      <SideBar className={"sidebar"}></SideBar>
+      <Leaflet className={"leaflet"} />
     </div>
-  )
+  );
 }
